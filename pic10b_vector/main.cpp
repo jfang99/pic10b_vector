4d997a5bd6d18951327d88755235df454462b51c
Jfei <Jfei@macdemacbook-pro-2.local>
2019-01-16 22:14:19 -0800

    Initial Commit

---
+++ pic10b_vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pic10b_vector
+//
+//  Created by Jfei on 2019/1/16.
+//  Copyright © 2019 Jfei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
